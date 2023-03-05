377fbede52c671ad8b27215b89d0b0c3201a6d73
eunjoo-kang <eunjoo.kang@nhnpayco.com>
2023-03-05 21:05:45 +0900

    init commit

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,36 @@
+package tobyspring.helloboot;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class HellobootApplication {
+
+    public static void main(String[] args) {
+//        new Tomcat().start();
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello, " + name);
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
+    }
+
+}
