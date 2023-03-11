b9b2a47d7c47cde7feecd6500221d5578f7005d1
joomn11 <joomn11@gmail.com>
2023-03-11 11:51:47 +0900

    토비의 스프링 - 독립형 실행형 스프링 애플리케이션
    - 스프링 컨테이너 생성 및 빈 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,10 +17,15 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
 //        new Tomcat().start();
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-            HelloController helloController = new HelloController();
+//            HelloController helloController = new HelloController();
 
             servletContext.addServlet("front-controller", new HttpServlet() {
                 @Override
@@ -28,10 +33,13 @@ public class HellobootApplication {
                     // 인증, 보안, 다국어, 공통 기능
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // mapping
                         String name = req.getParameter("name"); // binding
+
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//                        resp.setStatus(HttpStatus.OK.value());
+//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
 
