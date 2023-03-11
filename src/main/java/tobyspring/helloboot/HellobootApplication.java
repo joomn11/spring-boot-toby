4dc9f638f554f0076ea327bf512dd55bfac608b2
joomn11 <joomn11@gmail.com>
2023-03-11 19:11:09 +0900

    토비의 스프링 - 독립형 실행형 스프링 애플리케이션
    - 애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,24 +1,17 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
 //        new Tomcat().start();
 
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
@@ -27,29 +20,9 @@ public class HellobootApplication {
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
 //            HelloController helloController = new HelloController();
-
-            servletContext.addServlet("front-controller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) { // mapping
-                        String name = req.getParameter("name"); // binding
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-//                        resp.setStatus(HttpStatus.OK.value());
-//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else if (req.getRequestURI().equals("/user")) {
-
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                                      new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         webServer.start();
     }
