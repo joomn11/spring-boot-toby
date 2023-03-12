77cb287bd18583d2ed0918a7c2207e31a130bb8b
joomn11 <joomn11@gmail.com>
2023-03-12 10:40:38 +0900

    토비의 스프링 - 독립형 실행형 스프링 애플리케이션
    - @Component scan

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,10 +2,9 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -15,7 +14,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
