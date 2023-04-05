77ccbb4f1b74b94dbdf13e3a79b241ec53de6c0b
joomn11 <joomn11@gmail.com>
2023-04-05 19:46:29 +0900

    토비의 스프링 - 자동 구성 기반 애플리케이션
    - proxy bean option

--- src/test/java/tobyspring/study/ConfigurationTest.java
+++ src/test/java/tobyspring/study/ConfigurationTest.java
@@ -1,7 +1,9 @@
 package tobyspring.study;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,39 @@ public class ConfigurationTest {
     @Test
     void configuration() {
 
+//        MyConfig myConfig = new MyConfig();
+//        Bean1 bean11 = myConfig.bean1();
+//        Bean2 bean21 = myConfig.bean2();
+//        Assertions.assertThat(bean11.common).isSameAs(bean21);
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) {
+                this.common = super.common();
+            }
+            return this.common;
+        }
     }
 
     @Configuration
